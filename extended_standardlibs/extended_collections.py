f97d73cb685ff1cb87cc456beead993c3eb35477
Bert de Bruijn <lpdebruijn@gmail.com>
2024-12-29 16:26:55 -0400

    Add MaybeHashDict for mixed normal dict and NoHashDict

--- extended_standardlibs/extended_collections.py
+++ extended_standardlibs/extended_collections.py
@@ -2,17 +2,22 @@
 # - Two-way DictList ( "x['a'] += 'b'" and "x['b'] += 'a'" do the same thing, so Dict in usage, but more like a graph in working)
 # - NoHashDict (using lists to store a dict if you can't hash) -> can be used as if it is a dict but it's not
 #       Go through to see what to reimplement https://docs.python.org/3/library/stdtypes.html#typesmapping
+from itertools import chain
+
 
 class NoHashDict:
 
-    def __init__(self, list_tuple):
-        self._keys, self._values = list(zip(*list_tuple))
+    def __init__(self, list_tuple=None):
+        if list_tuple is None:
+            self._keys, self._values = tuple(), tuple()
+        else:
+            self._keys, self._values = list(zip(*list_tuple))
 
     def __str__(self):
-        return "{" + ", ".join([f"{k}: {v}" for k,v in zip(self._keys, self._values)]) + "}"
+        return "{" + ", ".join([f"{k}: {v}" for k,v in self.items()]) + "}"
 
     def __repr__(self):
-        return "{" + ", ".join([f"{k}: {v}" for k,v in zip(self._keys, self._values)]) + "}"
+        return self.__str__()
 
     def __len__(self):
         return len(self._keys)
@@ -29,7 +34,7 @@ class NoHashDict:
 
     def get(self, key, default=None):
         try:
-            return self._values[self._keys.index(key)]
+            return self[key]
         except ValueError:
             return default
 
@@ -58,10 +63,9 @@ class NoHashDict:
 
     def setdefault(self, key, default=None):
         if key in self._keys:
-            return self._values[self._keys.index(key)]
+            return self[key]
         else:
-            self._keys += (key, )
-            self._values += (default, )
+            self[key] = default
             return default
 
     def update(self, m, **kwargs):
@@ -89,7 +93,94 @@ class NoHashDict:
         return item in self._keys
 
     def __iter__(self):
-        return iter(self._keys)
+        return self.keys()
 
     def __reversed__(self):
         return reversed(self._keys)
+
+
+class MaybeHashDict(NoHashDict):
+
+    def __init__(self, list_tuple=None):
+        super().__init__()
+        self._dict = {}
+        self._nodict = NoHashDict()
+        if list_tuple is not None:
+            for k, v in list_tuple:
+                if self.hashash(k):
+                    self._dict[k] = v
+                else:
+                    self._nodict[k] = v
+
+    @staticmethod
+    def hashash(obj):
+        return hasattr(obj, '__hash__') and getattr(obj, '__hash__') is not None
+
+    def __len__(self):
+        return len(self._dict) + len(self._nodict)
+
+    def clear(self):
+        self._dict.clear()
+        self._nodict.clear()
+
+    def copy(self):
+        new = MaybeHashDict()
+        new._dict = self._dict.copy()
+        new._nodict = self._nodict.copy()
+        return new
+
+    @staticmethod
+    def fromkeys(iterable, value):
+        return MaybeHashDict(zip(iterable, iter(lambda: value, None)))
+
+    def items(self):
+        return chain(self._dict.items(), self._nodict.items())
+
+    def keys(self):
+        yield from self._dict.keys()
+        yield from self._nodict.keys()
+
+    def values(self):
+        yield from self._dict.values()
+        yield from self._nodict.values()
+
+    def pop(self, __key):
+        if self.hashash(__key):
+            return self._dict.pop(__key)
+        else:
+            return self._nodict.pop(__key)
+
+    def popitem(self):
+        if len(self._nodict) > 0:
+            return self._nodict.popitem()
+        else:
+            return self._dict.popitem()
+
+    def __setitem__(self, key, value):
+        if self.hashash(key):
+            self._dict[key] = value
+        else:
+            self._nodict[key] = value
+
+    def __getitem__(self, item):
+        return self._dict[item] if self.hashash(item) else self._nodict[item]
+
+    def __contains__(self, item):
+        return item in (self._dict if self.hashash(item) else self._nodict)
+
+    def __reversed__(self):
+        yield from reversed(self._nodict)
+        yield from reversed(self._dict)
+
+
+test1 = NoHashDict([(3, 2), (4, 1), (['koi'], 'koi')])
+print(test1)
+
+test3 = MaybeHashDict([(3, 2), (4, 1), (['koi'], 'koi')])
+print(test3)
+
+for k,v in test3.items():
+    print(k, v)
+
+for k in reversed(test3):
+    print(k, test3[k])
