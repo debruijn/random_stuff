f0f115f2750a3c1eb7247c603a91baffa35e11ca
Bert de Bruijn <lpdebruijn@gmail.com>
2024-12-30 21:04:15 -0400

    Add fn derangements_range to rust_playground

--- rust_playground/src/main.rs
+++ rust_playground/src/main.rs
@@ -1,6 +1,58 @@
 use std::collections::HashMap;
 use std::ops::Range;
+use std::time::Instant;
 use itertools::Itertools;
+
+
+fn derangements_range(n: usize) -> Vec<Vec<usize>> {
+    match n {
+        2 => vec![vec![1, 0]],
+        1 => Vec::new(),
+        0 => Vec::new(),
+        _ => {
+            let mut derangements = Vec::new();
+            let lag1 = derangements_range(n - 1);
+            for lag in lag1.iter() {
+                for split in 0..lag.len() {
+                    let mut temp = lag.iter().enumerate().
+                        map(|x| if x.0 == split {n-1} else { *x.1 }).collect_vec();
+                    temp.push(lag[split]);
+                    derangements.push(temp);
+                }
+            }
+
+            let lag2 = derangements_range(n - 2);
+            for lag in lag2.iter() {
+                let mut temp = lag.clone();
+                let mut temp2 = lag.clone();
+                temp.push(n-1);
+                temp.push(n-2);
+                derangements.push(temp);
+
+                for k in (0..n-2).rev() {
+                    // Todo single passthrough of temp2
+                    temp2 = temp2.into_iter().map(|x| if x == k {k+1} else {x}).collect_vec();
+                    let mut temp = Vec::new();
+                    for (i, el) in temp2.iter().enumerate() {
+                        if i == k {
+                            temp.push(n-1);
+                        }
+                        temp.push(*el)
+                    }
+                    if k == temp2.len() {
+                        temp.push(n-1)
+                    }
+                    temp.push(k);
+
+                    derangements.push(temp);
+                }
+            }
+
+            derangements
+        }
+    }
+}
+
 
 fn main() {
     let this: Range<usize> = 0..4;
@@ -16,7 +68,6 @@ fn main() {
 
     println!("Permutations: {:?}", perms);
     println!("Derangements: {:?}", derangs);
-
 
     let this: [usize;4] = [0, 0, 1, 2];
     let mut excl: HashMap<usize, Vec<usize>> = HashMap::new();
@@ -45,4 +96,60 @@ fn main() {
     println!("Derangements: {:?}", derangs);
     println!("Self derangements: {:?}", self_derangs);
     println!("Excl derangements: {:?}", excl_derangs);
+
+    let n = 10;
+
+    {
+        // let this: Range<usize> = 0..N;
+        let this: [usize;10] = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3];
+        let before = Instant::now();
+        let mut perms = Vec::new();
+        for i in this.into_iter().permutations(n) {
+            perms.push(i);
+        }
+        let after = Instant::now();
+        println!("Permutations: {:?} in {:?}", perms.len(), after - before);
+    }
+    {
+        let this: [usize;10] = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3];
+        let before = Instant::now();
+        let mut perms = Vec::new();
+        for i in this.into_iter().permutations(n).unique() {
+            perms.push(i);
+        }
+        let after = Instant::now();
+        println!("Distinct permutations: {:?} in {:?}", perms.len(), after - before);
+    }
+    {
+        // let this: Range<usize> = 0..N;
+        let this: [usize;10] = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3];
+        let before = Instant::now();
+        let mut derangs = Vec::new();
+        for i in this.into_iter().permutations(n) {
+            if !i.iter().enumerate().any(|x| x.0 == *x.1) {
+                derangs.push(i);
+            }
+        }
+        let after = Instant::now();
+        println!("Derangements: {:?} in {:?}", derangs.len(), after - before);
+    }
+    {
+        // let this: Range<usize> = 0..N;
+        let this: [usize;10] = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3];
+        let before = Instant::now();
+        let mut derangs = Vec::new();
+        for i in this.into_iter().permutations(n).unique() {
+            if !i.iter().enumerate().any(|x| x.0 == *x.1) {
+                derangs.push(i);
+            }
+        }
+        let after = Instant::now();
+        println!("Distinct derangements: {:?} in {:?}", derangs.len(), after - before);
+    }
+    {
+        let before = Instant::now();
+        let derangs = derangements_range(10);
+        let after = Instant::now();
+        println!("Derangements range: {:?} in {:?}", derangs.len(), after - before);
+    }
 }
